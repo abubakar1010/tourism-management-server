9b6cbff58d5a9b2bed708643f8cead3569a74ce6
Abubakar1010 <abubakar850772@gmail.com>
2024-05-02 16:46:59 +0600

    finished update operation

--- index.js
+++ index.js
@@ -50,12 +50,12 @@ async function run() {
       res.send(result);  
     });
 
-    // app.get("/tourists/update/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: new ObjectId(id) };
-    //   const result = await touristsCollection.findOne(query);
-    //   res.send(result);
-    // });
+    app.get("/tourists/update/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await touristsCollection.findOne(query);
+      res.send(result);
+    });
 
     app.post("/tourists", async (req, res) => {
       const newTourist = req.body;
@@ -64,29 +64,29 @@ async function run() {
       res.send(result);
     });
 
-    // app.put("/tourists/update/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const filter = { _id: new ObjectId(id) };
-    //   const option = { upsert: true };
-    //   const updateTourists = req.body
-    //   const tourists = {
-    //     $set: {
-    //       name: updateTourists.name,
-    //       email: updateTourists.email,
-    //       spotName: updateTourists.spotName,
-    //       countryName: updateTourists.countryName,
-    //       image: updateTourists.image,
-    //       location: updateTourists.location,
-    //       cost: updateTourists.cost,
-    //       season: updateTourists.season,
-    //       time: updateTourists.time,
-    //       visitor: updateTourists.visitor,
-    //       description: updateTourists.description,
-    //     },
-    //   };
-    //   const result = touristsCollection.updateOne(filter, tourists, option)
-    //   res.send(result)
-    // });
+    app.put("/tourists/update/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const option = { upsert: true };
+      const updateTourists = req.body
+      const tourists = {
+        $set: {
+          name: updateTourists.name,
+          email: updateTourists.email,
+          spotName: updateTourists.spotName,
+          countryName: updateTourists.countryName,
+          image: updateTourists.image,
+          location: updateTourists.location,
+          cost: updateTourists.cost,
+          season: updateTourists.season,
+          time: updateTourists.time,
+          visitor: updateTourists.visitor,
+          description: updateTourists.description,
+        },
+      };
+      const result = touristsCollection.updateOne(filter, tourists, option)
+      res.send(result)
+    });
 
 
     // app.delete("/tourists/delete/:id", async (req, res) => {
